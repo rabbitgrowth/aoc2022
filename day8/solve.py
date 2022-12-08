9ec947fe2fb54a9f66514fae73941252e6abcbf0
Nick Yeow <ytknick@gmail.com>
2022-12-08 22:19:25 +0800

    Day 8 in Python

---
+++ day8/solve.py
@@ -0,0 +1,43 @@
+with open('input.txt') as f:
+    trees = [list(map(int, line.rstrip())) for line in f]
+
+WIDTH  = len(trees[0])
+HEIGHT = len(trees)
+
+visible   = 0
+max_score = 0
+
+for y, row in enumerate(trees):
+    for x, tree in enumerate(row):
+        lines = []
+        for dx, dy in ((0, -1), (0, 1), (-1, 0), (1, 0)):
+            line = []
+            xx, yy = x, y
+            while True:
+                xx += dx
+                yy += dy
+                if not (0 <= xx < WIDTH and 0 <= yy < HEIGHT):
+                    break
+                line.append(trees[yy][xx])
+            lines.append(line)
+
+        for line in lines:
+            for other_tree in line:
+                if other_tree >= tree:
+                    break
+            else:
+                visible += 1
+                break
+
+        score = 1
+        for line in lines:
+            distance = 0
+            for distance, other_tree in enumerate(line, 1):
+                if other_tree >= tree:
+                    break
+            score *= distance
+        if score > max_score:
+            max_score = score
+
+print(visible)
+print(max_score)
