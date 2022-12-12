56312a91b1b50582e9fbd3f91de2fd15c9cd42f8
Nick Yeow <ytknick@gmail.com>
2022-12-12 21:16:45 +0800

    Day 11 in Python

---
+++ day11/solve.py
@@ -0,0 +1,42 @@
+import math
+
+class Monkey:
+    def __init__(self, items, operation, divisor, targets):
+        self.items     = items
+        self.operation = operation
+        self.divisor   = divisor
+        self.targets   = targets
+        self.inspections = 0
+
+with open('input.txt') as f:
+    monkeys = []
+    for par in f.read().split('\n\n'):
+        a, b, c, d, e = par.splitlines()[1:]
+        items     = list(map(int, a.split(': ')[1].split(', ')))
+        operation = b.split()[-2:]
+        divisor   = int(c.split()[-1])
+        targets   = [int(line.split()[-1]) for line in (d, e)]
+        monkeys.append(Monkey(items, operation, divisor, targets))
+
+product = math.prod(monkey.divisor for monkey in monkeys)
+
+for rounds, divide in ((20, True), (10000, False)):
+    for _ in range(rounds):
+        for i, monkey in enumerate(monkeys):
+            monkey.inspections += len(monkey.items)
+            for item in monkey.items:
+                match monkey.operation:
+                    case ('*', 'old'):
+                        item **= 2
+                    case ('*', x):
+                        item *= int(x)
+                    case ('+', x):
+                        item += int(x)
+                if divide:
+                    item //= 3
+                else:
+                    item %= product
+                target = monkeys[monkey.targets[bool(item % monkey.divisor)]]
+                target.items.append(item)
+            monkey.items.clear()
+    print(math.prod(sorted(monkey.inspections for monkey in monkeys)[-2:]))
